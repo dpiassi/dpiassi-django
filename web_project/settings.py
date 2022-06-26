bb9089c65446597c54fccf603c8c69c376261720
Daniel Piassi <daniel.piassi@outlook.com>
2022-06-25 23:28:36 -0300

    Fixed security issue by storing SECRET_KEY locally

--- web_project/settings.py
+++ web_project/settings.py
@@ -11,16 +11,17 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/4.0/howz\to/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i@3ny3t++9avl86id4r1e(ljp(=2b5fvis692szz*tvktc1^md'
+SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
